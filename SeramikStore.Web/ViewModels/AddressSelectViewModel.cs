d9ef71b282cdfe7a89d712f4c0dcfd691e31611a
mcngrl <mehmetcangurel@gmail.com>
2026-01-06 16:49:22 +0300

    Index Sayfası Güncellendi.

---
+++ SeramikStore.Web/ViewModels/AddressSelectViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SeramikStore.Web.ViewModels
+{
+    public class AddressSelectViewModel
+    {
+        public List<UserAddressViewModel> Addresses { get; set; }
+        public int? SelectedAddressId { get; set; }
+
+        public decimal ProductTotal { get; set; }
+        public decimal CargoPrice { get; set; }
+        public decimal GrandTotal => ProductTotal + CargoPrice;
+    }
+}
