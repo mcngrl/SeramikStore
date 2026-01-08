a7246271e8915a8a0812e72ad7c412ebb8ccfe05
mcngrl <mehmetcangurel@gmail.com>
2026-01-08 17:54:11 +0300

    DBScripter eklendi

---
+++ DBScripter/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System.Security.AccessControl;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+
+string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+try
+{
+
+	Scripter.Main(connectionString);
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message);
+}
