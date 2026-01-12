371300edb05cca33c24ac97fe50c8e2e2d02775b
mcngrl <mehmetcangurel@gmail.com>
2026-01-12 11:47:25 +0300

    Entity Creator eklendi.

---
+++ EntityCreator/DbSchemaReader.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+public static class DbSchemaReader
+{
+    public static List<DbColumn> GetColumns(
+        string connectionString,
+        string schema,
+        string table)
+    {
+        var list = new List<DbColumn>();
+
+        using SqlConnection conn = new(connectionString);
+        using SqlCommand cmd = new(@"
+            SELECT
+                c.name AS ColumnName,
+                t.name AS DataType,
+                c.is_nullable,
+                c.is_identity
+            FROM sys.columns c
+            JOIN sys.types t ON c.user_type_id = t.user_type_id
+            WHERE c.object_id = OBJECT_ID(@Table)
+            ORDER BY c.column_id
+        ", conn);
+
+        cmd.Parameters.AddWithValue("@Table", $"{schema}.{table}");
+        conn.Open();
+
+        using SqlDataReader rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+        {
+            list.Add(new DbColumn
+            {
+                Name = rdr["ColumnName"].ToString(),
+                SqlType = rdr["DataType"].ToString(),
+                IsNullable = (bool)rdr["is_nullable"],
+                IsIdentity = (bool)rdr["is_identity"]
+            });
+        }
+        return list;
+    }
+}
---
+++ EntityCreator/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Development.local.json", optional: false)
+    .Build();
+
+string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    Console.WriteLine("Connection string bulunamadı!");
+    return;
+}
+
+
+string solutionRoot = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.Parent!.FullName;
+
+string scriptsFolder = Path.Combine(solutionRoot, "SeramikStore.Entities");
+
+
+
+bool menu = true;
+string tabloadi = "";
+string outputFile = "";
+
+while (menu)
+{
+    Console.Clear();
+    tabloadi = "";
+    outputFile = "";
+    Console.WriteLine("CONNECTION STRING:");
+    Console.WriteLine(connectionString);
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Connection Stringdeki veritabanınındaki girilen Tablo için Entity Classi oluşacak.");
+    Console.WriteLine("--------------------");
+    Console.Write("Tablo Adı ? : ");
+    tabloadi = Console.ReadLine()?.Trim();
+
+    string fileName = $"{tabloadi}.cs";
+    outputFile = Path.Combine(scriptsFolder, fileName);
+    Console.WriteLine("HEDED DOSYA");
+    Console.WriteLine(outputFile);
+    Console.WriteLine();
+    Console.Write("DEVAM ET? (E/H): ");
+    var cevap = Console.ReadLine()?.Trim().ToUpper();
+
+    if (cevap == "E")
+        menu = false;
+    else if (cevap == "H")
+        return;
+}
+
+try
+{
+    EntityCreator.Creator.Main(connectionString,"dbo", tabloadi, tabloadi, "SeramikStore.Entities", outputFile);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata oluştu:");
+    Console.WriteLine(ex);
+    Console.ReadLine();
+}
+
+Console.WriteLine("BAŞARI İLE SONLANDI.");
+Console.WriteLine(tabloadi);
+Console.WriteLine(outputFile);
+
+Console.WriteLine("Porgramdan çıkmak için ENTER'a basınız.");
+Console.ReadLine();
