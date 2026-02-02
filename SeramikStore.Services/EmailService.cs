c03a146625426527a676b3acce43d8fa5d0427c0
mcngrl <mehmetcangurel@gmail.com>
2026-02-02 13:02:14 +0300

    EmailService Classı eklendi.

---
+++ SeramikStore.Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace SeramikStore.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailService(IOptions<EmailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public void Send(string to, string subject, string htmlBody)
+        {
+            var message = new MailMessage
+            {
+                From = new MailAddress(_settings.UserName, _settings.FromName),
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true
+            };
+
+            message.To.Add(to);
+
+            var client = new SmtpClient(_settings.Host, _settings.Port)
+            {
+                Credentials = new NetworkCredential(
+                    _settings.UserName,
+                    _settings.Password
+                ),
+                EnableSsl = _settings.EnableSsl
+            };
+
+            client.Send(message);
+        }
+    }
+}
---
+++ SeramikStore.Services/EmailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace SeramikStore.Services.Email
+{
+    public class EmailSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FromName { get; set; }
+        public string FromEmailAdress { get; set; }
+    }
+}
