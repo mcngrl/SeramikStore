6594f609b853a22c66143329d4ffb8c21c373e21
mcngrl <mehmetcangurel@gmail.com>
2026-02-27 17:51:24 +0300

    saassa

---
+++ SeramikStore.Web/Localization/ArabaResource.generated.cs
@@ -0,0 +1,13 @@
+
+namespace SeramikStore.Web.Localization
+{
+    /// <summary>
+    /// Localization marker class for Araba
+    /// Resource files:
+    ///  - ArabaResource.tr.resx
+    ///  - ArabaResource.en.resx
+    /// </summary>
+    public class ArabaResource
+    {
+    }
+}
