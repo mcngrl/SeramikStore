9f4d185c9e58952487c9094f6fb93104357ff2e7
mcngrl <mehmetcangurel@gmail.com>
2026-03-30 17:35:21 +0300

    İade tamamlandı.

--- SeramikStore.Contracts/Return/ReturnCreateViewDto.cs
+++ SeramikStore.Contracts/Return/ReturnCreateViewDto.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using SeramikStore.Contracts.Reason;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SeramikStore.Contracts.Return
-{
-    public class ReturnCreateViewDto
-    {
-        public int OrderId { get; set; }
-        public string Reason { get; set; }
-        public List<ReturnCreateItemDto> Items { get; set; }
-    }
-}
+//namespace SeramikStore.Contracts.Return
+//{
+//    public class ReturnCreateViewDto
+//    {
+//        public int OrderId { get; set; }
+//        public ReasonDto ReturnReason { get; set; }
+//        public List<ReturnCreateItemDto> Items { get; set; }
+
+//    }
+//}
