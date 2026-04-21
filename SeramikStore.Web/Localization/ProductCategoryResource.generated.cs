b437e4236bf10a6fc6a8c28858cb7ee73d26c171
mcngrl <mehmetcangurel@gmail.com>
2026-04-21 14:39:18 +0300

    pc

---
+++ SeramikStore.Web/Localization/ProductCategoryResource.generated.cs
@@ -0,0 +1,13 @@
+
+namespace SeramikStore.Web.Localization
+{
+    /// <summary>
+    /// Localization marker class for ProductCategory
+    /// Resource files:
+    ///  - ProductCategoryResource.tr.resx
+    ///  - ProductCategoryResource.en.resx
+    /// </summary>
+    public class ProductCategoryResource
+    {
+    }
+}
