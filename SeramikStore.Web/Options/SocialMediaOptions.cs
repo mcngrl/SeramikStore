1ec3e6aee2c09fc93b20e846b65b56033e220e78
mcngrl <mehmetcangurel@gmail.com>
2026-04-28 16:52:58 +0300

    wa

--- SeramikStore.Web/Options/SocialMediaOptions.cs
+++ SeramikStore.Web/Options/SocialMediaOptions.cs
@@ -3,6 +3,7 @@ namespace SeramikStore.Web.Options
     public class SocialMediaOptions
     {
         public string Instagram { get; set; }
+        public string WhatsApp { get; set; }
 
     }
 }
